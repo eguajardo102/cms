7efabd6d8abfcd4ae162d3c0a9805d454ec8382d
eguajardo102 <guajardo.erika@ymail.com>
2026-04-17 16:36:56 -0500

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,30 @@
+"""
+The flask application package.
+"""
+import logging
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_session import Session
+
+app = Flask(__name__)
+app.config.from_object(Config)
+streamHandler = logging.StreamHandler()
+streamHandler.setLevel(logging.WARNING)
+app.logger.addHandler(streamHandler)
+if log:
+if log == 'info':
+   app.logger.info('No issue.')
+elif log == 'warning':
+   app.logger.warning('Warning occurred.')
+elif log == 'error':
+   app.logger.error('Error occurred.')
+elif log == 'critical':
+   app.logger.critical('Critical error occurred.')
+Session(app)
+db = SQLAlchemy(app)
+login = LoginManager(app)
+login.login_view = 'login'
+
+import FlaskWebProject.views
---
+++ config.py
@@ -0,0 +1,41 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-key'
+
+    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'cms123'
+    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or 'mgQsBUm+BxGCyOQsmCoo5Wm0DxuzkTow++J8GDJ2mE6eDpO6xIyamm3H7QFYZtXiCHD1SDsqb5Us+AStGLmj8Q=='
+    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'images'
+
+    SQL_SERVER = os.environ.get('SQL_SERVER') or 'cms1.database.windows.net'
+    SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'cms'
+    SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'odl_user_298440@udacityhol.onmicrosoft.com'
+    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'tMekd6V@'
+    # Below URI may need some adjustments for driver version, based on your OS, if running locally
+    SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + '@' + SQL_SERVER + ':' + SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE  + '?driver=ODBC+Driver+17+for+SQL+Server'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    ### Info for MS Authentication ###
+    ### As adapted from: https://github.com/Azure-Samples/ms-identity-python-webapp ###
+    CLIENT_SECRET = "b7ccbd84-a456-4958-aa09-0716f8cab554"
+    # In your production app, Microsoft recommends you to use other ways to store your secret,
+    # such as KeyVault, or environment variable as described in Flask's documentation here:
+    # https://flask.palletsprojects.com/en/1.1.x/config/#configuring-from-environment-variables
+    # CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+    # if not CLIENT_SECRET:
+    #     raise ValueError("Need to define CLIENT_SECRET environment variable")
+
+    AUTHORITY = "https://login.microsoftonline.com/common"  # For multi-tenant app, else put tenant name
+    # AUTHORITY = "https://login.microsoftonline.com/Enter_the_Tenant_Name_Here"
+
+    CLIENT_ID = "e64e6ae0-d546-405b-8947-607181021295"
+
+    REDIRECT_PATH = "/getAToken"  # Used to form an absolute URL; must match to app's redirect_uri set in AAD
+
+    # You can find the proper permission names from this document
+    # https://docs.microsoft.com/en-us/graph/permissions-reference
+    SCOPE = ["User.Read"] # Only need to read user profile for this app
+
+    SESSION_TYPE = "filesystem"  # Token cache will be stored in server-side session
